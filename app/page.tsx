e2179f88b4c7e7f90a5350948fae6443b0edf662
calvinkorver <calvin.korver@gmail.com>
2024-07-17 20:23:54 -0400

    - Change title - Fix hover on projects coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,12 @@ export default function Home() {
             <h2 className="text-6xl text-white font-semibold sm:text-7xl">
               Kumo <span className="invisible lg:visible">Labs</span>
             </h2>
-            
+
             <div>
-            <h2 className="visible lg:invisible text-6xl font-medium tracking-tight text-white pt-2">Labs</h2>
-            </div> 
+              <h2 className="visible lg:invisible text-6xl font-medium tracking-tight text-white pt-2">
+                Labs
+              </h2>
+            </div>
             <p className="mt-8 text-base leading-6 text-gray-300 max-w-full">
               Showcasing innovative ideas & machine learning applications that{" "}
               <br></br>
@@ -33,12 +35,20 @@ export default function Home() {
               />
             </svg>
 
-            <p className="mt-6 font-medium text-lg leading-6 text-gray-300 max-w-full">
-              <a href="" className="hover:text-amber-300">
-                Projects
-              </a>
-              {/* <span className="text-sm"> (coming soon)</span> */}
-            </p>
+            <div className="group relative w-64">
+              <p className="mt-6 font-medium text-lg leading-6 text-gray-300 max-w-full">
+                <a
+                  href="#"
+                  className="relative inline-block ">
+                  <span className="block opacity-100 group-hover:opacity-0 transition-opacity duration-500">
+                    Projects
+                  </span>
+                  <span className="whitespace-nowrap absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 text-amber-300">
+                    Coming Soon
+                  </span>
+                </a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -54,16 +64,13 @@ export default function Home() {
           />
         </div>
         <div className="absolute bottom-[100px] right-[-85px] lg:bottom-0 lg:right-0 lg:translate-x-1/2 lg:translate-y-1/ lg:invisible visible">
-        <Image
-          src="/cloud_export.png"
-          alt="Cloud"
-          width={450}
-          height={200}
-        />
-      </div>
+          <Image src="/cloud_export.png" alt="Cloud" width={450} height={200} />
+        </div>
       </div>
       <div className="fixed bottom-0 left-0 p-6">
-        <p className="text-xs">© Calvin</p>
+        <a href="https://www.calvinkorver.com">
+          <p className="text-xs">© Calvin</p>
+        </a>
       </div>
     </main>
   );
