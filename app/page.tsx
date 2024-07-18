438dae8eab0d323c6a558b7bf617497405de39b6
Calvin <calvin.korver@gmail.com>
2024-07-17 22:34:25 -0400

    Bug: Fix Mobile Image (#2)
    * Priority
    
    * Package Lock, update Header
    
    * Fix header
    
    * Fix display
    
    * more adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,16 @@ export default function Home() {
       </div>
       <div className="hidden lg:flex lg:h-[17vh] w-full"></div>
       <div className="flex flex-row w-full">
-        <div className="lg:basis-1/3 basis-1/1 mt-32 lg:mt-0">
+        <div className="lg:basis-1/2 basis-1/1 mt-16 lg:mt-0">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-6xl text-white font-semibold sm:text-7xl">
-              Kumo <span className="invisible lg:visible">Labs</span>
-            </h2>
-
-            <div>
-              <h2 className="visible lg:invisible text-6xl font-medium tracking-tight text-white pt-2">
+              <span className="hidden lg:block">Kumo Labs</span>
+              <span className="block lg:hidden">
+                Kumo
+                <br />
                 Labs
-              </h2>
-            </div>
+              </span>
+            </h2>
             <p className="mt-8 text-base leading-6 text-gray-300 max-w-full">
               Showcasing innovative ideas & machine learning applications that{" "}
               <br></br>
@@ -37,9 +36,7 @@ export default function Home() {
 
             <div className="group relative w-64">
               <p className="mt-6 font-medium text-lg leading-6 text-gray-300 max-w-full">
-                <a
-                  href="#"
-                  className="relative inline-block ">
+                <a href="#" className="relative inline-block ">
                   <span className="block opacity-100 group-hover:opacity-0 transition-opacity duration-500">
                     Projects
                   </span>
@@ -52,7 +49,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="flex flex-row w-full invisible lg:visible">
+      <div className="flex flex-row w-full hidden lg:block">
         <div className="basis-1/2"></div>
         <div className="basis-1/2 flex justify-center lg:justify-end">
           <Image
@@ -61,12 +58,13 @@ export default function Home() {
             width={700}
             height={300}
             className="lg:mr-0"
+            priority={true}
           />
         </div>
-        <div className="absolute bottom-[100px] right-[-85px] lg:bottom-0 lg:right-0 lg:translate-x-1/2 lg:translate-y-1/ lg:invisible visible">
-          <Image src="/cloud_export.png" alt="Cloud" width={450} height={200} />
         </div>
-      </div>
+        <div className="absolute bottom-[25px] right-[-65px] lg:bottom-0 lg:right-0 lg:translate-x-1/2 lg:translate-y-1/ lg:hidden flex">
+          <Image src="/cloud_export.png" alt="Cloud" width={350} height={200} />
+        </div>
       <div className="fixed bottom-0 left-0 p-6">
         <a href="https://www.calvinkorver.com">
           <p className="text-xs">© Calvin</p>
